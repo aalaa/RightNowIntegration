2f55be61a9b30891bc2617c5b51929803b5771f6
ahosama <ahmed_osama@mentor.com>
2016-03-05 11:19:51 +0200

    Adding SampleSoftPhone project  to repository

---
+++ Avaya/Samples/softPhone_sample/Project/SampleSoftPhone/Avaya_Wrapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Nortel.CCT;
+
+namespace SampleSoftPhone
+{
+    class Avaya_Wrapper
+    {
+        private Toolkit cctToolKit;
+        private int lastError;
+
+        public Avaya_Wrapper()
+        {
+            cctToolKit = new Toolkit();
+
+            lastError = IProgErrors.E_SUCCESS;
+        }
+
+        public int login(String server, String user, String domain, String pwd)
+        {
+            // Validate parameters
+            if (server == null || server == "" ||
+                user == null || user == "" ||
+                domain == null || domain == "" ||
+                pwd == null || pwd == "")
+            {
+                IProgErrors.reportProdError(IProgErrors.E_INVALID_PARAM, String.Format(
+                    "Parameter values provided: Server: {0}, User: {1}, Domain: {2}, Password: {3}",
+                    server, user, domain, (pwd == null || pwd == "")? "Invalid Password" : "Valid Password"));
+            }
+
+            try
+            {
+                cctToolKit.Server = server;
+                cctToolKit.Port = 29373;
+                cctToolKit.Credentials = new CCTCredentials(user, domain, pwd);
+                cctToolKit.Connect();
+
+                lastError = IProgErrors.E_SUCCESS;
+            }
+            catch
+            {
+                IProgErrors.reportProdError(IProgErrors.E_LOGIN_FAILURE, "");
+
+                lastError = IProgErrors.E_LOGIN_FAILURE;
+            }
+
+            return lastError;
+        }
+    }
+}
---
+++ Avaya/Samples/softPhone_sample/Project/SampleSoftPhone/IProgErrors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleSoftPhone
+{
+    interface IProgErrors
+    {
+        public static int E_SUCCESS = 0;
+        public static int E_INVALID_PARAM = 0x02;
+        public static int E_LOGIN_FAILURE = 0x10;
+
+        public static Dictionary<int, String> errorLookUp = new Dictionary<int, string>();
+
+        private static String prodErrorMsg = "Error Code: {0}. Message: {1}. Extra Information: {3}";
+
+        public static void initLookUp()
+        {
+            if (errorLookUp.Count > 0)
+                return;
+
+            errorLookUp[E_LOGIN_FAILURE] = "";
+        }
+
+        public static void reportProdError(int error, String extraInfo)
+        {
+            String msg = String.Format(prodErrorMsg, error, errorLookUp[error], extraInfo);
+            Console.WriteLine(msg);
+        }
+
+        public static void reportDevError(int error, String extraInfo)
+        {
+        }
+    }
+}
